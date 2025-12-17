52f245ed27a19f1f0a25ea8c26a3fea7d19d1278
shashwat <shashwat@duby.com>
2025-12-18 00:17:19 +0530

    areaofcircle

---
+++ src/shashwat/java/basic/areaofcircle.java
@@ -0,0 +1,12 @@
+package shashwat.java.basic;
+
+public class areaofcircle {
+    public static void main(String[] args) {
+        float radius = 7.2f;
+        float PI = 3.14f;
+        float area=PI*radius*radius;
+        System.out.println(area);
+    }
+
+
+}
