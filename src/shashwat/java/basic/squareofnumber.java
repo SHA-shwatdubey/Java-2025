521d179e148666fc5a8f22bbbb93873cb9ad8724
shashwat <shashwat@duby.com>
2025-12-17 23:53:33 +0530

    squareofnumber

---
+++ src/shashwat/java/basic/squareofnumber.java
@@ -0,0 +1,9 @@
+package shashwat.java.basic;
+
+public class squareofnumber {
+    public static void main(String[] args) {
+        int num1=3;
+        int sqr=num1*num1;
+        System.out.println(sqr);
+    }
+}
