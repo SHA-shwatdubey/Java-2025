281dd4b679365444e075ba040834e73912a773f6
shashwat <shashwat@duby.com>
2025-12-18 00:09:31 +0530

    areaofrectangle

---
+++ src/shashwat/java/basic/areaofrectangle.java
@@ -0,0 +1,11 @@
+package shashwat.java.basic;
+
+public class areaofrectangle {
+    public static void main(String[] args) {
+        float length = 4.3f;
+        float breath = 4.2f;
+        float area = length*breath;
+        System.out.println(area);
+
+    }
+}
