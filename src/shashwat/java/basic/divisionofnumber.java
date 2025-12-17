3fec9c6587da52d961393030617e01864f8c9c52
shashwat <shashwat@duby.com>
2025-12-17 23:42:55 +0530

    divisionofnumber

---
+++ src/shashwat/java/basic/divisionofnumber.java
@@ -0,0 +1,11 @@
+package shashwat.java.basic;
+
+public class divisionofnumber {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        int div=num2/num1;
+        System.out.println(div);
+
+    }
+}
