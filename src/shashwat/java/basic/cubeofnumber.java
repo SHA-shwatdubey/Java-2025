3acbdfa683916c1ecb5ccafa0f9211ec0a51dc93
shashwat <shashwat@duby.com>
2025-12-18 00:04:22 +0530

    cubeofnumber

---
+++ src/shashwat/java/basic/cubeofnumber.java
@@ -0,0 +1,10 @@
+package shashwat.java.basic;
+
+public class cubeofnumber {
+    public static void main(String[] args) {
+        int num1=6;
+        int cube=num1*num1*num1;
+        System.out.println(cube);
+
+    }
+}
