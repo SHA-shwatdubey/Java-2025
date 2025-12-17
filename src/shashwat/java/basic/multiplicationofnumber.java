1a10cb684b140f603df7353b2f0156ee12198768
shashwat <shashwat@duby.com>
2025-12-17 23:38:43 +0530

    multiplicationofnumber

---
+++ src/shashwat/java/basic/multiplicationofnumber.java
@@ -0,0 +1,10 @@
+package shashwat.java.basic;
+
+public class multiplicationofnumber {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        int mul=num1*num2;
+        System.out.println(mul);
+    }
+}
