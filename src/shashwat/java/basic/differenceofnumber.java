be2033217490e44bdeaddbfaef1263bb471fd7a1
shashwat <shashwat@duby.com>
2025-12-17 23:25:34 +0530

    differenceofnumber

---
+++ src/shashwat/java/basic/differenceofnumber.java
@@ -0,0 +1,14 @@
+package shashwat.java.basic;
+
+public class differenceofnumber {
+    public static void main(String[] args) {
+        int num1=20;
+        int num2=10;
+        int sub=num1-num2;
+        System.out.println(sub);
+
+
+    }
+
+
+}
