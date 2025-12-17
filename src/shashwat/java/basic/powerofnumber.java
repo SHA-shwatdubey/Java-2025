17fb9d3da58be08da34efa53b1e4b2e5ff54a305
shashwat <shashwat@duby.com>
2025-12-18 00:00:55 +0530

    powerofnumber

---
+++ src/shashwat/java/basic/powerofnumber.java
@@ -0,0 +1,12 @@
+package shashwat.java.basic;
+
+
+public class powerofnumber {
+    public static void main(String[] args) {
+        int num1=2;
+        int num2=5;
+        int power=num1*num1*num1*num1*num1;
+        System.out.println(power);
+    }
+
+}
