b4991df21170416931e52a177d1794a9e8b7e4a1
shashwat <shashwat@duby.com>
2025-12-18 00:13:43 +0530

    perimeterofrectangle

---
+++ src/shashwat/java/basic/perimeterofrectangle.java
@@ -0,0 +1,11 @@
+package shashwat.java.basic;
+
+public class perimeterofrectangle {
+    public static void main(String[] args) {
+        float length =20.3f;
+        float breath = 22.34f;
+        float perimeter = (length+breath)*2;
+        System.out.println(perimeter);
+
+    }
+}
